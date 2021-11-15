11a77bc0768fb0cf2ca19c8839ae8d98358ad41f
BasantaKumar Sahoo <basantakumar.sahoo@capgemini.com>
2021-11-15 10:57:40 +0000

    Initial Checkin

---
+++ OneClickDevOpsGithub/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OneClickDevOpsGithub.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
