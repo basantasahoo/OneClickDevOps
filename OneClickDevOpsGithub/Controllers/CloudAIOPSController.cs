32547b19d299f38bfe8e22ded7b06843194c33a9
Basanta Kumar Sahoo <basanta_sahoo@hotmail.com>
2021-12-07 15:10:40 +0000

    Added Cloud AI OPS and Monitor functionality

---
+++ OneClickDevOpsGithub/Controllers/CloudAIOPSController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OneClickDevOpsGithub.Controllers
+{
+    public class CloudAIOPSController : Controller
+    {
+        public IActionResult AIMotnitor()
+        {
+            return View();
+        }
+    }
+}
