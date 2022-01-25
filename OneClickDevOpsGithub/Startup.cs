f7464d715318c04e35f94ffad566763719be4d50
Atul Sharma <atul.g.sharma@capgemini.com>
2022-01-25 15:59:37 +0000

    Added Swagger for APIs

--- OneClickDevOpsGithub/Startup.cs
+++ OneClickDevOpsGithub/Startup.cs
@@ -24,6 +24,7 @@ namespace OneClickDevOpsGithub
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,13 +32,17 @@ namespace OneClickDevOpsGithub
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                //app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+               // app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+               // app.UseHsts();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
